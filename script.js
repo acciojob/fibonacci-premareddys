179cab14a47f9b295272531fd05781daddb55f0d
Siddareddygari Prema Reddy <92963246+premareddys@users.noreply.github.com>
2023-11-23 19:42:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function fibonacci(num) {
 // your code here
-	let f1=0;
-	let f2=1;
+	let num1=0;
+	let num2=1;
 	let sum=0;
-	for(let i=2;i<num;i++){
-		sum = f1 +f2;
-		return sum;
-		f1=f2;
-		f2=f3;
-	}
+	for(let i=2;i<num;++i){
+		sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+    }
+    return num2;
 }
 
 module.exports = fibonacci;
