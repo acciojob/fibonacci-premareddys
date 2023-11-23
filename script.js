5fd281ed13d40f225ae8ad7bcb2d51f16806e18d
Siddareddygari Prema Reddy <92963246+premareddys@users.noreply.github.com>
2023-11-23 21:06:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ function fibonacci(num) {
 	else{
 	let num1=0;
 	let num2=1;
-	let sum=0;
+	
 	for(let i=2;i<=num;i++){
-		sum = num1 + num2;
+		let sum = num1 + num2;
         num1 = num2;
         num2 = sum;
     }
